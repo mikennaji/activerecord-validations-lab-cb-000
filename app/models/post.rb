cc480088d025c5f0f6ce38fb0e827d4495494bda
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-02 14:22:41 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
       message: "%{value} is not a valid category" }
+  validate :clickbaity?    
       CLICKBAITY = [
          /Won't Believe/i,
          /Secret/i,
